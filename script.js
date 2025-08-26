f47ffd81ad88c25415ec530a03396431c30f2354
Rodrigo <rodrigopdq@live.com>
2025-08-25 21:12:36 -0300

    feat: Implementa lógica do botão para gerar citação aleatória

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const citacoes = [
 "A vida é o que acontece enquanto você está ocupado fazendo outros planos. - John Lennon", 
 "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. - Winston Churchill" 
 ]; 
+const citacaoEl = document.getElementById("citacao"); 
+const botaoEl = document.getElementById("novaCitacaoBtn"); 
+botaoEl.addEventListener("click", () => { 
+const randomIndex = Math.floor(Math.random() * citacoes.length); 
+citacaoEl.textContent = citacoes[randomIndex]; 
+});
